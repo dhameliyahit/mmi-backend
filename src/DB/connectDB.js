d86407820ca11653222921089317e6c3df4b2d29
dhameliyahit <heetdhameliya59@gmail.com>
2024-09-15 18:54:43 +0530

    add url

--- src/DB/connectDB.js
+++ src/DB/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.DB_URL);
+      await mongoose.connect(process.env.DB_URL || "mongodb+srv://heetdhameliya59:yLQrb7qqY4EUfA6G@mmi.ggllc.mongodb.net/mmi");
       console.log('MongoDB connected successfully');
     } catch (error) {
       console.error('Error connecting to MongoDB:', error.message);
